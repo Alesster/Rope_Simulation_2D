5173b363b03583314a966b9701e63a4b82d53a64
Alesster <aless.ter@gmail.com>
2024-05-19 20:18:55 +0200

    Add new classes

--- main.cpp
+++ main.cpp
@@ -15,29 +15,37 @@ public:
 	double getRho() { return RHO; }
 	double getPi() { return PI; }
 	double getG() { return g; }
-
-	double KA = 0.4;			// Resistant coefficient for element of mass of Rope
-	double KAend = 0.5;			// Resistant coefficient for mass at the end of Rope
-	double R = 0.02;			// Radious of Rope section
-	double Srope = PI * R * R;	// Square of Rope section
-	double Rend = 0.05;			// Radious of end mass section
-	double Send = PI * Rend * Rend;	//Square of end mass section
 };
 
+class ParamRope:public Param
+{
+public:
+	double KA = 0.4;			// Resistant coefficient for element of mass of Rope
+	double Rrope = 0.02;			// Radious of Rope section
+	double Srope = getPi() * Rrope * Rrope;	// Square of Rope section
+
+	double CoefRope = KA * getRho() * Srope / 2;
+};
+
+class ParamMass:public Param
+{
+public:
+	double KAmass = 0.5;			// Resistant coefficient for mass at the end of Rope
+	double Rmass = 0.05;			// Radious of end mass section
+	double Smass = getPi() * Rmass * Rmass;	//Square of end mass section
+
+	double CoefMass = KAmass * getRho() * Smass / 2;
+};
 
 int main()
 {
 	Param param;
+	ParamRope prope;
+	ParamMass pmass;
+	
 	double const RHO = param.getRho();	// air density
 	double const PI = param.getPi();
 	double const g = param.getG();		// Gravitational acceleretaion
-
-	double KA = param.KA;			// Resistant coefficient for element of mass of Rope
-	double KAend = param.KAend;			// Resistant coefficient for mass at the end of Rope
-	double R = param.R;			// Radious of Rope section
-	double Srope = param.Srope;	// Square of Rope section
-	double Rend = param.Rend;			// Radious of end mass section
-	double Send = param.Send;	//Square of end mass section
 	
 	//Uncomment if you want enter data from console 
 	/*int NUM;
@@ -63,8 +71,8 @@ int main()
 	int NUM = 20;			// Number of point masses of Rope
 	double length = 0.05;	// Length of point mass of Rope in m
 	double mass = 0.02;		// Mass of point element of Rope in kg
-	double endMass = 0.2;	// Mass at the end element of Rope in kg
-	double airvel = 50;		// Speed of Air Flow in m/s
+	double endMass = 0.3;	// Mass at the end element of Rope in kg
+	double airvel = 30;		// Speed of Air Flow in m/s
 	double gamma = 10;		// Angle of Air Flow with respect to horizen
 	int deltaON = 0;		// Switch ON (1) or OFF (0) ocasional perturbation
 
@@ -76,7 +84,8 @@ int main()
 	double gammaRad = gamma * PI / 180;	// Angle of airflow with respect to horizon (minus if down, plus if up) 
 	double flowCos = cos(gammaRad);
 	double flowSin = sin(gammaRad);
-	double AirForce = KA * RHO * airvel * airvel * Srope / 2;	// Airflow resistance of element of Rope
+	//double AirForce = KA * RHO * airvel * airvel * Srope / 2;	// Airflow resistance of element of Rope
+	double AirForce = prope.CoefRope * airvel * airvel;
 	double Weight = g * mass;						// Weight of element of Rope
 	double RXconst = AirForce * flowCos;			// Projection of Force to X axis
 	double RYconst = -Weight + AirForce * flowSin;	// Projection of Force to Y axis
@@ -92,7 +101,8 @@ int main()
 	}
 	else
 	{
-		AirForceEnd = KAend * RHO * airvel * airvel * Send / 2;
+		//AirForceEnd = KAend * RHO * airvel * airvel * Send / 2;
+		AirForceEnd = pmass.CoefMass * airvel * airvel;
 		endWeight = g * endMass;
 	}
 
@@ -103,7 +113,6 @@ int main()
 		int sign = pow(-1, rnum);
 		double randnum = ((double)rand() / RAND_MAX)*sign/100;
 		double delta = randnum * log(airvel + 1) * deltaON;
-		//cout << delta << endl;
 
 		if (i == NUM-1) 
 		{
@@ -143,8 +152,8 @@ int main()
 	cout << "Mass of Rope: " << mass * (NUM - 1) + endWeight/g << " kg" << endl;
 	cout << "Air Flow speed: " << airvel << " m/s, Angle of Air Flow: " << gamma << " deg" <<  endl;
 	cout << "Perturbation: " << deltaON << endl;
-	cout << "Coordinates of points of Rope" << endl;
-	cout << " Angle" << " \t \t " << "X" << " \t \t " << "Y" << endl;
+	cout << "Coordinates of points of Rope:" << endl;
+	cout << "Angle Phi" << " \t " << "X" << " \t \t " << "Y" << endl;
 	for (int i = 0; i < NUM; i++)
 	{
 		cout << fixed << setprecision(6) << asin(vsin[i])*180/PI << " \t " << xcoor[i] << " \t " << ycoor[i] << endl;
